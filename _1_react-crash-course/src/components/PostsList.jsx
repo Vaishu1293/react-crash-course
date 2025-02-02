8d5a8ca46880cc89b18dd1178dd27dc22478ba6d
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-02 18:13:14 +0000

    loading-state-management

--- _1_react-crash-course/src/components/PostsList.jsx
+++ _1_react-crash-course/src/components/PostsList.jsx
@@ -7,12 +7,15 @@ import { useState, useEffect } from 'react';
 function PostsList({ isPosting, onStopPosting }) {
 
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts');
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
         fetchPosts();
     }, []);
@@ -48,17 +51,18 @@ function PostsList({ isPosting, onStopPosting }) {
 
     return (<>
         {modalContent}
-        {posts.length > 0 && (<ul className={classes.posts}>
+        {!isFetching && posts.length > 0 && (<ul className={classes.posts}>
             {posts.filter(post => post).map((post) => (
                 <Post key={post.body} author={post.author} body={post.body} />
             ))}
         </ul>)}
-        {posts.length === 0 && (
+        {!isFetching && posts.length === 0 && (
             <div style={{textAlign: 'center', color: 'white'}}>
                 <h2>There are no posts yet.</h2>
                 <p>Start adding some!</p>
             </div>
         )}
+        {isFetching && <div style={{ textAlign: 'center', color: 'white' }}><p>Loading posts...</p></div>}
     </>);
 }
 
