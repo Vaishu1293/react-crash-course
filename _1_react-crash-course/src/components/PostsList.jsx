7ef92aab17bc18bed2f4072e65bfa2193cf89987
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-02 14:11:53 +0000

    modal

--- _1_react-crash-course/src/components/PostsList.jsx
+++ _1_react-crash-course/src/components/PostsList.jsx
@@ -3,13 +3,14 @@ import { useState } from 'react';
 import Post from './Post';
 import classes from './PostsList.module.css';
 import NewPost from './NewPost';
+import Modal from './Modal';
 
 function PostsList() {
-
+    const [ modalIsVisible, setModalIsVisible ] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setAuthor] = useState('');
 
-    function bodyChangeHandler(event){
+    function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
     }
 
@@ -17,8 +18,20 @@ function PostsList() {
         setAuthor(event.target.value);
     }
 
-    return (<>
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    let modalContent;
+
+    if(modalIsVisible){
+        modalContent = <Modal onClose={hideModalHandler}>
         <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+    </Modal>
+    }
+
+    return (<>
+        {modalContent}
         <ul className={classes.posts}>
             <Post author={enteredAuthor} body={enteredBody} />
             {/* <Post author="Chandrashekhar" body="Check out the full course!" /> */}
