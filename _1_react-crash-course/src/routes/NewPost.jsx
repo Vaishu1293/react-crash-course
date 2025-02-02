b345ff8d0c15ddc9ed4654ca2ad39dd29a69a57d
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-02 20:29:36 +0000

    linking-navigating

--- _1_react-crash-course/src/routes/NewPost.jsx
+++ _1_react-crash-course/src/routes/NewPost.jsx
@@ -1,8 +1,9 @@
 import classes from './NewPost.module.css';
 import { useState } from 'react';
 import Modal from '../components/Modal';
+import { Link } from 'react-router-dom';
 
-function NewPost({ onCancel, onAddPost }) {
+function NewPost({ onAddPost }) {
 
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setAuthor] = useState('');
@@ -39,12 +40,12 @@ function NewPost({ onCancel, onAddPost }) {
         <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
-        <button type="button" onClick={onCancel}>Cancel</button>
+        <Link type="button" to=".." className={classes.button}>Cancel</Link>
         <button>Submit</button>
       </p>
     </form>
     </Modal>
-  );
+  ); 
 }
 
 export default NewPost;
