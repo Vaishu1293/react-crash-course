dc4b42398690916dee06eb588707697219a33739
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-02 08:04:49 +0000

    event-listeners

--- _1_react-crash-course/src/components/PostsList.jsx
+++ _1_react-crash-course/src/components/PostsList.jsx
@@ -4,11 +4,11 @@ import NewPost from './NewPost';
 
 function PostsList() {
     return (<>
-    <NewPost/>
-    <ul className={classes.posts}>
-        <Post author="Vaishali" body="React.js is awesome!" />
-        <Post author="Chandrashekhar" body="Check out the full course!" />
-    </ul>
+        <NewPost />
+        <ul className={classes.posts}>
+            <Post author="Vaishali" body="React.js is awesome!" />
+            <Post author="Chandrashekhar" body="Check out the full course!" />
+        </ul>
     </>);
 }
 
