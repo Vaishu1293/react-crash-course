0714f725403bfcbe390968a3496f295b3947cf5a
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-04 20:27:19 +0000

    post-details-page

--- _1_react-crash-course/src/components/Post.jsx
+++ _1_react-crash-course/src/components/Post.jsx
@@ -1,16 +1,18 @@
 import classes from './Post.module.css';
-
+import { Link } from 'react-router-dom';
 
 // const names = ['Vaishali', 'Chandrashekhar', 'Sharmila', 'Shivakumar', 'Pammy'];
 
-function Post({author, body}) {
+function Post({id, author, body}) {
 
     // const chosenName =  Math.random() > 0.5 ? names[0] : names [1];
 
     return (
         <li className={classes.post}>
+            <Link to={id}>
             <p className={classes.author}>{author}</p>
             <p className={classes.text}>{body}</p>
+            </Link>
         </li>
     );
 }
--- _1_react-crash-course/src/main.jsx
+++ _1_react-crash-course/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails';
 import Posts, { loader as postsLoader } from './routes/Posts';
 import NewPost, {action as newPostAction} from './routes/NewPost';
 import RootLayout from './routes/RootLayout';
@@ -16,7 +16,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Posts />,
         loader: postsLoader,
-        children: [{ path: '/create-post', element: <NewPost />, action: newPostAction }],
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:postId', element: <PostDetails />, loader: postDetailsLoader }
+        ],
       },
     ],
   },
